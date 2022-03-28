d59e47fbf0aa7738f64aae6514b374ddccaf10e8
parag naik <naikparag@gmail.com>
2022-03-28 22:51:52 +0530

    widget: Editor and Explore widget inside a column layout

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:nitrogen/widgets/editor.dart';
+import 'package:nitrogen/widgets/explorer.dart';
 
 class DesktopBody extends StatelessWidget {
   const DesktopBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.deepPurple[300]);
+    return Scaffold(
+        body: Row(
+      children: [
+        Container(
+          child: const Explorer(),
+          color: Colors.white,
+          constraints: const BoxConstraints(minWidth: 240),
+        ),
+        const VerticalDivider(
+          width: 20,
+          thickness: 2,
+        ),
+        Expanded(
+          child: Container(
+            child: const Editor(),
+            color: Colors.grey,
+          ),
+        )
+      ],
+    ));
   }
 }
